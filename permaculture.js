744b383a65cd47f09ce435a2bce713e34d8714e3
Ezra Edgerton <ezraedgerton@MacBook-Pro-68.local>
2020-11-16 10:31:49 -0600

    preliminary plans

--- permaculture.js
+++ permaculture.js
@@ -14,11 +14,42 @@ function toggler(e){
   }
 }
 
+var prev_selected;
+
+function feature_toggler(e){
+	console.log("feature_toggler")
+	let this_id = $(this).attr("id")
+	if (prev_selected == this_id){
+		$('#' + this_id + '_div').addClass("hidden")
+		prev_selected = undefined;
+		return
+	}
+	if (prev_selected == undefined){
+		prev_selected = this_id
+		$('#' + this_id + '_div').removeClass("hidden")
+		return
+	}
+	$('#' + prev_selected + '_div').addClass("hidden")
+	$('#' + this_id + '_div').removeClass("hidden")
+	prev_selected = this_id
+	return
+
+}
+
 $('#trees').on("click", toggler)
 $('#water').on("click", toggler)
 $('#roads').on("click", toggler)
 $('#paths').on("click", toggler)
 $('#structures').on("click", toggler)
+$('#solarization').on("click", toggler)
+
+
+
+let features_ids = ["shed", "path", "windbreak", "orchard", "riparian", "keyhole", "grasses", "invasive", "long_term"]
+
+features_ids.forEach(function(d){
+	$("#" + d).on("click", feature_toggler);
+})
 
 }
 
